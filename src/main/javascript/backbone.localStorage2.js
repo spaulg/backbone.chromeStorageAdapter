c80613ba507910a912c92821890e4f9da1778e47
Simon Paulger <spaulger@codezen.co.uk>
2013-08-10 20:57:36 +0100

    implement storage methods

--- src/main/javascript/backbone.localStorage2.js
+++ src/main/javascript/backbone.localStorage2.js
@@ -19,17 +19,208 @@
     'use strict';
 
     /**
-     * localStorage Backbone storage adapter
+     * Chrome Storage Backbone storage adapter
      *
      * @param keyNamespace storage namespace prefix
+     * @param storageArea Storage area to use
      * @constructor
      */
-    Backbone.LocalStorage2 = function(keyNamespace) {
-        this._keyNamespace = keyNamespace;
+    Backbone.ChromeStorage = function(keyNamespace, storageArea) {
+        if (typeof keyNamespace == 'string' && keyNamespace != '') {
+            this._keyNamespace = keyNamespace;
+        } else {
+            throw new TypeError('Invalid key namespace.');
+        }
+
+        // Apply storage area
+        if (storageArea == 'local' || storageArea == null) {
+            this._chromeStorage = chrome.storage.local;
+        } else if (storageArea == 'sync') {
+            this._chromeStorage = chrome.storage.sync;
+        } else {
+            throw new TypeError('Invalid storage area declaration.');
+        }
     };
 
-    Backbone.LocalStorage2.prototype = {
+    /**
+     * Enum for StorageArea type. Should be when declaring
+     * the storage area when constructing the storage adapter
+     */
+    Backbone.ChromeStorage.StorageArea = {
+        SYNC: 'sync',
+        LOCAL: 'local'
+    };
+
+    Backbone.ChromeStorage.prototype = {
         _keyNamespace: null,
+        _chromeStorage: null,
+        _recordIndex: [],
+
+        /**
+         * Get the Chrome storage namespace pointer
+         *
+         * @returns {Object} Chrome storage namespace object
+         * @private
+         */
+        _getChromeStorage: function()
+        {
+            return this._chromeStorage;
+        },
+
+        /**
+         * Generate a unique UUID for use as a key
+         *
+         * @returns {string} Unique UUID
+         * @private
+         */
+        _generateUuid: function()
+        {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        },
+
+        /**
+         * Low level API set call
+         *
+         * @param data Data to set
+         * @param callback Callback on completion
+         * @private
+         */
+        _set: function(data, callback)
+        {
+            this._getChromeStorage().set(data, callback);
+        },
+
+        /**
+         * Low level API get call
+         *
+         * @param keys Keys of data to retrieve
+         * @param callback Callback on completion
+         * @private
+         */
+        _get: function(keys, callback)
+        {
+            this._getChromeStorage().get(keys, callback);
+        },
+
+        /**
+         * Low level API remove call
+         *
+         * @param keys Keys to remove
+         * @param callback Callback on completion
+         * @private
+         */
+        _remove: function(keys, callback)
+        {
+            this._getChromeStorage().remove(key, callback);
+        },
+
+        /**
+         * Add or update an existing key/value pair in storage
+         *
+         * @param model Model of data to update
+         * @param options Storage options
+         * @private
+         */
+        _updateModel: function(model, options)
+        {
+            function apiCallback() {
+                if (chrome.runtime.lastError == null && options.callback != null) {
+                    options.callback.call(model);
+                } else if (options.errback != null) {
+                    options.errback.call(model);
+                }
+            }
+
+            // Get or generate an Id
+            var id = model.id || this._generateUuid();
+
+            // Update record index with new id if not found
+            if ($.inArray(id, this._recordIndex) == -1) {
+                this._recordIndex.push(id);
+            }
+
+            // Update index if adding, and add/update record
+            var data = [];
+            data[this._keyNamespace] = this._recordIndex;
+            data[id] = model.attributes;
+            this._set(data, apiCallback);
+
+            // Set id to model
+            model.id = id;
+        },
+
+        _deleteModel: function(model, options)
+        {
+            var callbackCount = 0;
+            var success = true;
+            var errors = [];
+
+            function apiCallback() {
+                callbackCount++;
+
+                // Check if an error occurred
+                if (chrome.runtime.lastError != null) {
+                    success = false;
+                    errors.push(chrome.runtime.lastError);
+                }
+
+                // Notify callback if defined
+                if (callbackCount == 2 && success && options.callback != null) {
+                    // Success
+                    options.callback.call(model);
+                } else if (callbackCount == 2) {
+                    // Error
+                    options.errback.call(model, errors);
+                }
+            }
+
+            // If the model has an id, attempt to remove it from storage
+            if (model.id != null) {
+                // Find record index position and remove
+                var recordPosition = $.inArray(model.id, this._recordIndex);
+                if (recordPosition != -1) {
+                    this._recordIndex.splice(recordPosition, 1);
+                }
+
+                // Update record index with item removed in storage
+                var data = [];
+                data[this._keyNamespace] = this._recordIndex;
+                this._set(data, apiCallback);
+
+                // Remove item from storage
+                this._remove([model.id], apiCallback);
+            } else {
+                // Nothing to remove, notify errback if defined
+                if (options.errback != null) {
+                    options.errback('Model id is not defined');
+                }
+            }
+        },
+
+        _readModel: function(model, options)
+        {
+            function apiCallback(items) {
+                if (chrome.runtime.lastError == null) {
+                    // Update all the model attributes with the items retrieved
+                    model.set(items[model.id]);
+
+                    // Notify callback if defined
+                    if (options.callback != null) {
+                        options.callback.call(model);
+                    }
+                } else if (options.errback != null) {
+                    options.errback.call(model, chrome.runtime.lastError);
+                }
+            }
+
+            // If the model has an id, attempt to retrieve it from storage
+            if (model.id != null) {
+                this._get([model.id], apiCallback);
+            }
+        },
 
         /**
          * Minimum storageProxy plugin interface requirement conforming
@@ -40,7 +231,12 @@
          * @param options Options
          */
         sync: function(method, model, options) {
-
+            switch (method) {
+                case 'update':
+                case 'create':  return this._updateModel(model, options);
+                case 'read':    return this._readModel(model, options);
+                case 'delete':  return this._deleteModel(model, options);
+            }
         }
     };
 })();
